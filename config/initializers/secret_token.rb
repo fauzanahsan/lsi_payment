ff846fb87d80fe6aa24a80927060e241f58da4ff
Fauzan Ahsan <fauzan@Fauzans.local>
2012-11-18 22:30:04 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsiPayment::Application.config.secret_token = '87332fdfdc2fe82925133d8ddbfd5941e4f515b1b0a30c81085d1b0f7b0112ea3ed2d28908dbc696a8c0d80c0bd4873783f84e33461aa337b426a175f7ff9e5d'
